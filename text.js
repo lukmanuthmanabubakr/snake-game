d476bedde11f6f2d34b78fb8be88b2f0763aeec6
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-03 12:03:43 +0100

    first commit

---
+++ text.js
@@ -0,0 +1,21 @@
+// maths method is an intrinsic method that provides basic mathematics functionality.
+
+// let x = 3.19;
+// x = Math.round(x)
+// x = Math.floor(x)
+// x = Math.ceil(x)
+// x = Math.pow(x, 2)
+// x = Math.sqrt(x)
+// x = Math.abs(x)
+// console.log(x)
+
+
+
+// let x = 3.19;
+// let y = 5;
+// let z = 9
+
+// maximum = Math.max (x, y, z);
+// minimum = Math.min(x, y, z);
+// console.log(minimum)
+// console.log(maximum)
